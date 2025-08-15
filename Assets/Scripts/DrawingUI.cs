8f6df4d172454f54e44c5fc877d1cd9e10d3bd0a
Luckless Fox <forick007@gmail.com>
2025-08-15 12:43:01 +0300

    Added UI

---
+++ Assets/Scripts/DrawingUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawingUI : MonoBehaviour
+{
+    [SerializeField] private FingerDrawingSystem _drawingSystem;
+    [SerializeField] private Button _colorButton;
+    [SerializeField] private Button _saveButton;
+    [SerializeField] private Button _loadButton;
+    [SerializeField] private Button _clearButton;
+
+    private Color[] _colors = { Color.red, Color.blue };
+    private int _currentColorIndex;
+
+    private void Start()
+    {
+        _colorButton.onClick.AddListener(ToggleColor);
+        //_saveButton.onClick.AddListener();
+        //_loadButton.onClick.AddListener();
+        _clearButton.onClick.AddListener(_drawingSystem.ClearAll);
+    }
+
+    private void ToggleColor()
+    {
+        _currentColorIndex = (_currentColorIndex + 1) % _colors.Length;
+        _drawingSystem.SetColor(_colors[_currentColorIndex]);
+    }
+}
